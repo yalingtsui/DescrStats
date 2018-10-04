e09466580f568205ab2d5291dcc318e0bc09d77d
Yaling <39233172+yalingtsui@users.noreply.github.com>
2018-10-04 17:29:25 -0500

    Update DescrStat.py

--- DescrStat.py
+++ DescrStat.py
@@ -37,3 +37,9 @@ def DescrStat(datafile):
     data_mean = data.mean()
     data_std = data.std()
     data_var = data.var()
+    
+    print(’Mean of the data is:’, data_mean)
+    print(’Standard deviation of the data is:’, data_std)
+    print(’Variance of the data is:’, data_var)
+    
+    return data_mean, data_std, data_var
